93fb55acfb72f57a73c043495860d825ddfef526
Adam Marshall <adam@appsynergy.net>
2017-03-16 14:07:22 +0000

    colors, table, select leader

--- src/vues/Select/Factions.vue
+++ src/vues/Select/Factions.vue
@@ -1,43 +1,54 @@
 <template>
   <div class="select-factions-vue mt-4">
-    
+
     <h2>Select your faction(s)</h2>
-    
-    <button class="btn mr-2 mt-2"
-      v-for="faction in factions"
+
+    <button v-for="faction in factions"
+      :class="['btn mr-2 mt-2', faction, isSelected(faction)?'selected':null]"
       v-on:click="toggleFaction(faction)"
-      v-bind:class="{ selected: isSelected(faction) }"
     >{{ faction }}</button>
-    
+
   </div>
 </template>
 
 <script lang="coffee">
 
   SelectFactions =
-    
+
     props: [ 'factions' ]
-    
+
     data: () ->
       selectedFactions: []
-      
+
     methods:
-      
+
       toggleFaction: (faction) ->
         if @isSelected faction
-          @selectedFactions = _.without @selectedFactions, faction 
+          @selectedFactions = _.without @selectedFactions, faction
         else
           @selectedFactions.push faction
         @$emit 'selectedFactions', @selectedFactions
 
       isSelected: (faction) ->
         _.contains @selectedFactions, faction
-        
-  export default SelectFactions  
+
+  export default SelectFactions
 </script>
 
 <style lang="sass">
-  button
-    &.selected
-      background-color: #dee
+
+  $colors: (Guild: #c73130, Resurrectionist: #008d47, Neverborn: #7f4996, Arcanist: #374ea2, Outcast: #bda93e, TenThunders: #d6742a, Gremlin: #8b692b)
+
+  =malifauxFactions
+    @each $faction, $color in $colors
+      &.#{$faction}
+        background-color: $color
+
+  .select-factions-vue
+    button
+      +malifauxFactions
+      &:focus
+        box-shadow: none
+      &.selected
+        color: white
 </style>
--- src/vues/Select/Leader.vue
+++ src/vues/Select/Leader.vue
@@ -1,44 +1,74 @@
 <template>
   <div class="select-leader-vue mt-4">
-    
+
     <h2>Select your leader</h2>
-    
+
     <form class="form-inline my-4">
       <label class="mr-4" for="soulstones">Soulstones: </label>
       <input type="text" class="form-control" v-model="soulstones">
     </form>
-      
-    <li v-for="leader in leaders">
-      {{ leader.name }} - cache: {{ leader.cache }}
-    </li>
-    
+
+    <table class="table table-bordered table-hover" v-if="hasLeaders">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Cache</th>
+          <th>Keywords</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="leader in leaders"
+          v-on:click="selectLeader(leader)"
+          :class="{ selected: selectedLeader == leader }">
+          <td>{{ leader.name }}</td>
+          <td>{{ leader.cache }}</td>
+          <td>{{ listAttributes(leader.attributes, ', ') }}</td>
+        </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
 
 <script lang="coffee">
-  
+
   SelectLeader =
-    
+
     props: [ 'masters', 'henchmen', 'factions' ]
-    
+
     data: () ->
-      soulstones: 35
-    
+      soulstones: 50
+      selectedLeader: null
+
     computed:
+      hasLeaders: () -> @leaders.length > 0
       leaders: () ->
         out = []
         if @soulstones > 25 then @filterByFaction @masters, out
         if @soulstones < 40 then @filterByFaction @henchmen, out
         _.flatten out
-        
+
     methods:
-      
+
+        selectLeader: (leader) ->
+          @selectedLeader = leader
+
         filterByFaction: (models, results) ->
           _.map models, (factiongroup, faction) =>
             if _.contains @factions, faction
               results.push factiongroup
             else
               results
-        
+
+        listAttributes: (atts, interp) ->
+          _.reject atts, (x) ->
+            x.substring(0,4) == "Wave" || x == "Master"
+          .join interp
+
   export default SelectLeader
 </script>
+
+<style lang="sass">
+  tr.selected td
+    background-color: #ccc
+</style>
